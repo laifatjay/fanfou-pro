0aef8a28f3565bc7bbdfa019ee6a9298bce5e301
LitoMore <litomore@gmail.com>
2020-03-17 21:19:45 +0800

    Updates

---
+++ precache-manifest.fcd5102908425fc48fff3ad13213574e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f985c2c185a24f77fe9c82f5f69753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6991eb6f21df9564ef42",
+    "url": "/static/css/2.29be9311.chunk.css"
+  },
+  {
+    "revision": "2a884a865ab24ecd8a36",
+    "url": "/static/css/main.546b2790.chunk.css"
+  },
+  {
+    "revision": "6991eb6f21df9564ef42",
+    "url": "/static/js/2.da4ae4c0.chunk.js"
+  },
+  {
+    "revision": "bca055fc4dbca9dd1aa14293a6bc338f",
+    "url": "/static/js/2.da4ae4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a884a865ab24ecd8a36",
+    "url": "/static/js/main.e4499664.chunk.js"
+  },
+  {
+    "revision": "97f61e6b3c9b32b06e0f",
+    "url": "/static/js/runtime-main.024071f3.js"
+  },
+  {
+    "revision": "ad9117f0838b6e6b3c41cf3ac84356cf",
+    "url": "/static/media/login-badge-1.ad9117f0.svg"
+  },
+  {
+    "revision": "a0e78eb485fe6ddaf6f6a1f54f5339e5",
+    "url": "/static/media/login-badge-2.a0e78eb4.svg"
+  },
+  {
+    "revision": "e7c076d492da29ee4b49a53a1d772e56",
+    "url": "/static/media/login-badge-3.e7c076d4.svg"
+  },
+  {
+    "revision": "b3d6fc376c51fd45e83fa44e09808722",
+    "url": "/static/media/logo.b3d6fc37.svg"
+  }
+]);
